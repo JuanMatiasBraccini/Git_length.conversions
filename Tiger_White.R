e800fd803ac39c4f84ea1ce927f7c34e920c7599
Matias Braccini <Matias.Braccini@fish.wa.gov.au>
2021-04-28 13:17:13 +0800

    Changed to OneNote wd

--- Tiger_White.R
+++ Tiger_White.R
@@ -1,5 +1,6 @@
 # Estimation of Tiger and White shark size after release
-handl_OneDrive=function(x)paste('C:/Users/myb/OneDrive - Department of Primary Industries and Regional Development/Matias',x,sep='/')
+if(!exists('handl_OneDrive')) source('C:/Users/myb/OneDrive - Department of Primary Industries and Regional Development/Matias/Analyses/SOURCE_SCRIPTS/Git_other/handl_OneDrive.R')
+
 setwd(handl_OneDrive("Analyses/Length conversions"))
 #Growth Parameters
 
